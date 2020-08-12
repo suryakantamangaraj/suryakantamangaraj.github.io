7027fae55f03346b44152bffe40a9939f5820c28
suryakantamangaraj <surya.socialnetworking@gmail.com>
2020-08-12 17:28:26 +0530

    Upadted external links

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,16 +12,6 @@ export class ContactComponent implements OnInit {
   contactProfile = {
     profile: [
       {
-        name: 'Facebook',
-        url: 'https://www.facebook.com/suryaraj.fb',
-        iconUrl: './assets/icons/facebook.svg'
-      },
-      {
-        name: 'Twitter',
-        url: 'https://twitter.com/_suryaraj_',
-        iconUrl: './assets/icons/twitter.svg'
-      },
-      {
         name: 'Instagram',
         url: 'https://www.instagram.com/suryaraj.ig/',
         iconUrl: './assets/icons/instagram.svg'
@@ -30,6 +20,16 @@ export class ContactComponent implements OnInit {
         name: 'LinkedIn',
         url: 'https://www.linkedin.com/in/suryakantamangaraj/',
         iconUrl: './assets/icons/linkedin.svg'
+      },
+      {
+        name: 'Facebook',
+        url: 'https://www.facebook.com/suryaraj.fb',
+        iconUrl: './assets/icons/facebook.svg'
+      },
+      {
+        name: 'Twitter',
+        url: 'https://twitter.com/_suryaraj_',
+        iconUrl: './assets/icons/twitter.svg'
       }
     ],
     work: [
@@ -39,19 +39,19 @@ export class ContactComponent implements OnInit {
         iconUrl: './assets/icons/github.svg'
       },
       {
-        name: 'Hackerrank',
-        url: 'https://www.hackerrank.com/_suryaraj_',
-        iconUrl: './assets/icons/hackerrank.svg'
+        name: 'Medium',
+        url: 'https://medium.com/@surya.raj',
+        iconUrl: './assets/icons/medium.svg'
       },
       {
-        name: 'Stackoverflow',
-        url: 'https://stackoverflow.com/story/suryaraj',
-        iconUrl: './assets/icons/stackoverflow.svg'
+        name: 'Behance',
+        url: 'https://www.behance.net/surya_raj',
+        iconUrl: './assets/icons/behance.svg'
       },
       {
-        name: 'Pluralsight',
-        url: 'https://pluralsight.com/profile/suryaraj',
-        iconUrl: './assets/icons/pluralsight.svg'
+        name: 'Goodreads',
+        url: 'https://www.goodreads.com/user/show/98923939-surya-raj',
+        iconUrl: './assets/icons/goodreads.svg'
       }
     ]
   };
