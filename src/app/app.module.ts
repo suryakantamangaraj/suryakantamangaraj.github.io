16e15a5f50d5f276178e9e15da93c647d4d80eb4
suryakantamangaraj <surya.socialnetworking@gmail.com>
2020-08-19 06:41:46 +0530

    re-build footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,7 @@ import { appRouting } from './app.routing';
 import { NoEmojiPipe } from './pipes/no-emoji.pipe';
 import { SkillsService } from './services/skills.service';
 import { AboutMoreComponent } from './aboutmore/aboutmore.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -52,7 +53,8 @@ import { AboutMoreComponent } from './aboutmore/aboutmore.component';
     TypingComponent,
     LandingPageComponent,
     NoEmojiPipe,
-    AboutMoreComponent
+    AboutMoreComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
