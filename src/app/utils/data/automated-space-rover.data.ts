2f77e3bc837860fd2785b2e90af1c36be1fa3e6b
suryakantamangaraj <surya.socialnetworking@gmail.com>
2020-08-22 20:43:32 +0530

    Project Updated

---
+++ src/app/utils/data/automated-space-rover.data.ts
@@ -0,0 +1,17 @@
+import { IProject } from '../IProject';
+
+export const AutomatedSpaceRoverData: IProject = {
+  id: '8d9056cb-e47e-11ea-a204-68ecc5e082cd',
+  name: 'Automated Space Rover',
+  description:
+    'Design of Automated Space Rover using FPGA ',
+  projectLink: null,
+  liveUrl: null,
+  isFeatured: false,
+  features: [
+    'Designed for space application'
+  ],
+  tags: ['semiconductor'],
+  techStuff: ['Xilinx ISE Design', 'FPGA'],
+  additionalData: []
+};
